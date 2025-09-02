f68db4f0d92b996552b49d58989655b81f6fd1ff
Chael250 <ganzac784@gmail.com>
2025-09-02 12:02:02 +0200

    initial commit

---
+++ Librarium/src/test/java/com/chael/Librarium/LibrariumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chael.Librarium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrariumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
